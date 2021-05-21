cdd9c9cba76ae70041d8472e3e58ae406fc00ef3
Kenta Yamada <51431248+kurikinton105@users.noreply.github.com>
2021-05-21 15:59:50 +0900

    2まで

--- kyamada/chapter02/01.py
+++ kyamada/chapter02/01.py
@@ -1,3 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+fs = 44100 # サンプリング周波数
+t = np.arange(fs/2-1)/fs
+x = np.sin(2*np.pi*t)
+
+S = np.sum(x)/fs
+print(S)
+
+plt.plot(t,x)
+plt.xlabel("Time (s)")
+plt.ylabel("Amplitude")
+plt.show()
