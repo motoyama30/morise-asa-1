ed8d64b8f66a6b73b17f2e8070c81e9159585f57
bobtenshi <hei8maji@gmail.com>
2021-05-21 17:52:27 +0900

    2章を追加

---
+++ syamaji/chapter02/03.py
@@ -0,0 +1,26 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy.lib.function_base import angle
+import math
+
+def task2_3():
+  #定義
+  N = 1000
+  x1 = np.random.randn(N)
+  x2 = np.random.randn(N)
+
+  x1 = x1 - np.mean(x1)
+  x2 = x2 - np.mean(x2)
+
+  L_p = 10*np.log10(np.sum(x1**2)/np.sum(x2**2))
+  print('L_p : {}' .format(L_p))
+
+
+if __name__ == '__main__':
+
+  task2_3()
+
+
+
+
