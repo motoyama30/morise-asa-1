f41755c849cdad431969148534204b9aad95f348
y.kuriki <kuriki.yui@gmail.com>
2021-05-23 12:00:03 +0900

    パラメータの位置を修正

--- ykuriki/chapter02/05.py
+++ ykuriki/chapter02/05.py
@@ -1,18 +1,20 @@
 import numpy as np
 
 
+##パラメータ
 fs = 100
-t = np.arange(0, fs)/fs
 f = 1
+snr = 6
+
+t = np.arange(0, fs)/fs
 signal = np.sin(2*np.pi*f*t)
 noise = np.random.randn(fs)
 
-print(10*np.log10(sum(signal**2)))
-print(10*np.log10(sum(noise**2)))
+print('power of signal:',10*np.log10(sum(signal**2)))
+print('power of signal:',10*np.log10(sum(noise**2)))
 
-snr = 6
 noise = noise/np.sqrt(sum(noise**2))
 noise = noise*np.sqrt(sum(signal**2))
 noise = noise*10**(-snr/20)
 
-print(10*np.log10(np.mean(signal**2)/np.mean(noise**2)))
+print('SNR            :',10*np.log10(np.mean(signal**2)/np.mean(noise**2)))
