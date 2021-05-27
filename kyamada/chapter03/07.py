928e21548b6108c7876947acd9b8d4f0c7adb962
Kenta Yamada <51431248+kurikinton105@users.noreply.github.com>
2021-05-27 22:09:28 +0900

    update 07

--- kyamada/chapter03/07.py
+++ kyamada/chapter03/07.py
@@ -1,36 +1,45 @@
 # 高速フーリエ変換によるスペクトル解析
 import numpy as np
 import matplotlib.pyplot as plt
-N = 8 # 信号長
-#np.random.seed(0) # シード値
-x = np.random.randn(N,1)
-t = np.arange(N).reshape((N, 1))
-x = np.sin(2*np.pi*2*t)
-print(x)
-print(x.shape)
-print(x)
-plt.plot(x)
-plt.show()
-c = np.zeros((N,1),dtype="complex128") # ゼロ埋めを行う(複素数範囲での配列の作成が必要)
-t = np.arange(N).reshape((N, 1)) # (1,fs)の縦ベクトル
-print(c)
-# DFTでの値の計算
-for i in range(N):
-    c[i] = np.sum(x*np.exp(-1j*2*np.pi*i*t/N))
-    print(np.sum(x*np.exp(-1j*2*np.pi*i*t))/N)
+np.random.seed(0) # シード値
+
+
+def CreateRandomSignal(N):
+    x = np.random.randn(N,1)
+    t = np.arange(N).reshape((N, 1))
+
+    return t,x
+
+def DFT(N,t,x):
+    c = np.zeros((N,1),dtype="complex128") # ゼロ埋めを行う(複素数範囲での配列の作成が必要)
+    t = np.arange(N).reshape((N,1)) # (N,1)の縦ベクトル
+
+    # DFTでの値の計算
+    for i in range(N):
+        c[i] = np.sum(x*np.exp(-1j*2*np.pi*i*t/N))
+
+    return c
+
+
+# FFT関数での値の計算(転置したとき)
+def FFT_T(x):
+    x = x.T # np.fft.fftの引数は、(1,8)
+    X = np.fft.fft(x)
+
+    return X.T
+
+if __name__ == '__main__':
+    N = 8 # 信号長
+    t,x = CreateRandomSignal(N)
+
+    c = DFT(N,t,x)
+    X = np.fft.fft(x,axis=0) # 軸の指定を行う
+    X2 = FFT_T(x)
+
+    print(c)
+    print(X)
+    # 値が近いかを調べる（誤差を含める）
+    print(np.isclose(c,X))
 
 
 
-# FFT関数での値の計算
-X = np.fft.fft(x)
-print(X.dtype)
-print(c)
-print(X)
-print(np.abs(c))
-print(np.abs(X))
-
-plt.subplot(2,1,1)
-plt.plot(np.abs(c))
-plt.subplot(2,1,2)
-plt.plot(np.abs(X))
-plt.show()
