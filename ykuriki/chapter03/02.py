6f5150329dbce645ea173962b7dad432fa3b75f2
y.kuriki <kuriki.yui@gmail.com>
2021-05-28 19:06:17 +0900

    フォーマット修正

--- ykuriki/chapter03/02.py
+++ ykuriki/chapter03/02.py
@@ -1,11 +1,16 @@
 import numpy as np
 
 
+<<<<<<< HEAD
 # パラメータ
+=======
+##パラメータ
+>>>>>>> ade158846085480320c13d37c2b26953dd679278
 fs = 44100
 r = 1.5
 theta = 0.3
 f = 2
+<<<<<<< HEAD
 L = 1 / f
 
 t = np.arange(0, fs * L) / fs
@@ -16,3 +21,15 @@ b = 2 / fs / L * sum(x * np.sin(2 * np.pi * t / L))
 
 print(f"amplitude: {np.sqrt(a**2+b**2):.10f}")
 print(f"phase    : {np.arctan2(b,a):.10f}")
+=======
+L = 1/f
+
+t = np.arange(0,fs*L)/fs
+x = r*np.cos(2*np.pi*t/L-theta)
+
+a = 2/fs/L*sum(x*np.cos(2*np.pi*t/L))
+b = 2/fs/L*sum(x*np.sin(2*np.pi*t/L))
+
+print(f'amplitude: {np.sqrt(a**2+b**2):.10f}')
+print(f'phase    : {np.arctan2(b,a):.10f}')
+>>>>>>> ade158846085480320c13d37c2b26953dd679278
--- ykuriki/chapter03/05.py
+++ ykuriki/chapter03/05.py
@@ -1,7 +1,11 @@
 import numpy as np
 
 
+<<<<<<< HEAD
 # パラメータ
+=======
+##パラメータ
+>>>>>>> ade158846085480320c13d37c2b26953dd679278
 fs = 44100
 f1 = 1
 r1 = 1.5
@@ -10,6 +14,7 @@ f2 = 3
 r2 = 0.2
 theta2 = -1.1
 
+<<<<<<< HEAD
 t = np.arange(0, fs) / fs
 x = r1 * np.cos(2 * np.pi * f1 * t - theta1) + r2 * np.cos(2 * np.pi * f2 * t - theta2)
 
@@ -24,3 +29,19 @@ print("phase    : {:.10f}".format(np.angle(c1.conjugate())))
 print("3[Hz]")
 print("amplitude: {:.10f}".format(abs(c3)))
 print("phase    : {:.10f}".format(np.angle(c3.conjugate())))
+=======
+t = np.arange(0,fs)/fs
+x = r1*np.cos(2*np.pi*f1*t-theta1)+r2*np.cos(2*np.pi*f2*t-theta2)
+
+k1 = 1
+c1 = sum(x*np.exp(-1j*2*np.pi*k1*t))/fs
+k3 = 3
+c3 = sum(x*np.exp(-1j*2*np.pi*k3*t))/fs
+
+print('1[Hz]')
+print('amplitude: {:.10f}'.format(abs(c1)))
+print('phase    : {:.10f}'.format(np.angle(c1.conjugate())))
+print('3[Hz]')
+print('amplitude: {:.10f}'.format(abs(c3)))
+print('phase    : {:.10f}'.format(np.angle(c3.conjugate())))
+>>>>>>> ade158846085480320c13d37c2b26953dd679278
