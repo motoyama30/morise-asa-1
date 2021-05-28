3695fc1c97ca25877e04e4f86445c0d1e98b76ad
sogyoten <skt.nishida@gmail.com>
2021-05-28 16:13:40 +0900

    3章を追加

---
+++ knishida/chapter03/04.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+
+#declare
+fs=44100
+n1=1
+r1=1.5
+theta1=0.3
+n2=3
+r2=0.3
+theta2=1.1
+t=np.arange(0,1,1/fs)
+x=np.array(r1*np.cos(2*np.pi*n1*t-theta1)+r2*np.cos(2*np.pi*n2*t-theta2))
+
+#calc about a,b
+a1=2/fs*np.sum(x*np.cos(2*np.pi*n1*t))
+b1=2/fs*np.sum(x*np.sin(2*np.pi*n1*t))
+a2=2/fs*np.sum(x*np.cos(2*np.pi*n2*t))
+b2=2/fs*np.sum(x*np.sin(2*np.pi*n2*t))
+
+#check
+r1_c=np.sqrt(a1**2+b1**2)
+theta1_c=np.arctan2(b1,a1)
+r2_c=np.sqrt(a2**2+b2**2)
+theta2_c=np.arctan2(b2,a2)
+
+#output
+print("r1:"+str(r1_c))
+print("theta1:"+str(theta1_c))
+print("r2:"+str(r2_c))
+print("theta2:"+str(theta2_c))
