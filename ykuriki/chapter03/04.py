f8f05eddf1b7588a82aaca706cec83848184feaa
y.kuriki <kuriki.yui@gmail.com>
2021-05-28 18:19:57 +0900

    blackでフォーマットを修正

--- ykuriki/chapter03/04.py
+++ ykuriki/chapter03/04.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-##パラメータ
+# パラメータ
 fs = 44100
 n1 = 1
 r1 = 1.5
@@ -10,21 +10,21 @@ n2 = 3
 r2 = 0.3
 theta2 = 1.1
 
-t = np.arange(0,fs)/fs
-x = r1*np.cos(2*np.pi*n1*t-theta1)+r2*np.cos(2*np.pi*n2*t-theta2)
+t = np.arange(0, fs) / fs
+x = r1 * np.cos(2 * np.pi * n1 * t - theta1) + r2 * np.cos(2 * np.pi * n2 * t - theta2)
 
-a = 2/fs*sum(x*np.cos(2*np.pi*t))
-b = 2/fs*sum(x*np.sin(2*np.pi*t))
+a = 2 / fs * sum(x * np.cos(2 * np.pi * t))
+b = 2 / fs * sum(x * np.sin(2 * np.pi * t))
 
-print('1[Hz]')
-print(f'amplitude: {np.sqrt(a**2+b**2):.10f}')
-print(f'phase    : {np.arctan2(b,a):.10f}')
+print("1[Hz]")
+print(f"amplitude: {np.sqrt(a**2+b**2):.10f}")
+print(f"phase    : {np.arctan2(b,a):.10f}")
 
 
-##3Hzに変えて実行
-a = 2/fs*sum(x*np.cos(2*np.pi*t*3))
-b = 2/fs*sum(x*np.sin(2*np.pi*t*3))
+# 3Hzに変えて実行
+a = 2 / fs * sum(x * np.cos(2 * np.pi * t * 3))
+b = 2 / fs * sum(x * np.sin(2 * np.pi * t * 3))
 
-print('3[Hz]')
-print(f'amplitude: {np.sqrt(a**2+b**2):.10f}')
-print(f'phase    : {np.arctan2(b,a):.10f}')
+print("3[Hz]")
+print(f"amplitude: {np.sqrt(a**2+b**2):.10f}")
+print(f"phase    : {np.arctan2(b,a):.10f}")
--- ykuriki/chapter03/05.py
+++ ykuriki/chapter03/05.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-##パラメータ
+# パラメータ
 fs = 44100
 f1 = 1
 r1 = 1.5
@@ -10,17 +10,17 @@ f2 = 3
 r2 = 0.2
 theta2 = -1.1
 
-t = np.arange(0,fs)/fs
-x = r1*np.cos(2*np.pi*f1*t-theta1)+r2*np.cos(2*np.pi*f2*t-theta2)
+t = np.arange(0, fs) / fs
+x = r1 * np.cos(2 * np.pi * f1 * t - theta1) + r2 * np.cos(2 * np.pi * f2 * t - theta2)
 
 k1 = 1
-c1 = sum(x*np.exp(-1j*2*np.pi*k1*t))/fs
+c1 = sum(x * np.exp(-1j * 2 * np.pi * k1 * t)) / fs
 k3 = 3
-c3 = sum(x*np.exp(-1j*2*np.pi*k3*t))/fs
+c3 = sum(x * np.exp(-1j * 2 * np.pi * k3 * t)) / fs
 
-print('1[Hz]')
-print('amplitude: {:.10f}'.format(abs(c1)))
-print('phase    : {:.10f}'.format(np.angle(c1.conjugate())))
-print('3[Hz]')
-print('amplitude: {:.10f}'.format(abs(c3)))
-print('phase    : {:.10f}'.format(np.angle(c3.conjugate())))
+print("1[Hz]")
+print("amplitude: {:.10f}".format(abs(c1)))
+print("phase    : {:.10f}".format(np.angle(c1.conjugate())))
+print("3[Hz]")
+print("amplitude: {:.10f}".format(abs(c3)))
+print("phase    : {:.10f}".format(np.angle(c3.conjugate())))
