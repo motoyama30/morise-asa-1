e120464fad4fe4c4fde1a6af75c21fa8ceb761c7
Yui Kuriki <73982429+sternbergia@users.noreply.github.com>
2021-05-29 01:14:50 +0900

    Update 04.py

--- ykuriki/chapter03/04.py
+++ ykuriki/chapter03/04.py
@@ -1,11 +1,7 @@
 import numpy as np
 
 
-<<<<<<< HEAD
 # パラメータ
-=======
-##パラメータ
->>>>>>> ade158846085480320c13d37c2b26953dd679278
 fs = 44100
 n1 = 1
 r1 = 1.5
@@ -14,7 +10,6 @@ n2 = 3
 r2 = 0.3
 theta2 = 1.1
 
-<<<<<<< HEAD
 t = np.arange(0, fs) / fs
 x = r1 * np.cos(2 * np.pi * n1 * t - theta1) + r2 * np.cos(2 * np.pi * n2 * t - theta2)
 
@@ -33,23 +28,3 @@ b = 2 / fs * sum(x * np.sin(2 * np.pi * t * 3))
 print("3[Hz]")
 print(f"amplitude: {np.sqrt(a**2+b**2):.10f}")
 print(f"phase    : {np.arctan2(b,a):.10f}")
-=======
-t = np.arange(0,fs)/fs
-x = r1*np.cos(2*np.pi*n1*t-theta1)+r2*np.cos(2*np.pi*n2*t-theta2)
-
-a = 2/fs*sum(x*np.cos(2*np.pi*t))
-b = 2/fs*sum(x*np.sin(2*np.pi*t))
-
-print('1[Hz]')
-print(f'amplitude: {np.sqrt(a**2+b**2):.10f}')
-print(f'phase    : {np.arctan2(b,a):.10f}')
-
-
-##3Hzに変えて実行
-a = 2/fs*sum(x*np.cos(2*np.pi*t*3))
-b = 2/fs*sum(x*np.sin(2*np.pi*t*3))
-
-print('3[Hz]')
-print(f'amplitude: {np.sqrt(a**2+b**2):.10f}')
-print(f'phase    : {np.arctan2(b,a):.10f}')
->>>>>>> ade158846085480320c13d37c2b26953dd679278
