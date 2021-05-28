467ec5d6145326265d41ddba3d51cdef1b514076
Kenta Yamada <51431248+kurikinton105@users.noreply.github.com>
2021-05-28 17:44:14 +0900

    blackへの変更（1,2章）

--- kyamada/chapter01/01.py
+++ kyamada/chapter01/01.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.array([1,3,-5,2])
+x = np.array([1, 3, -5, 2])
 
 plt.plot(x)
 plt.xlabel("Array Number")
@@ -10,9 +10,9 @@ plt.show()
 
 # 標本化周期を10Hzにして、横軸を秒にする
 fs = 10
-t = np.arange(len(x))/fs
+t = np.arange(len(x)) / fs
 print(t)
-plt.plot(t,x)
+plt.plot(t, x)
 plt.xlabel("Time (s)")
 plt.ylabel("Amplitude")
 plt.show()
--- kyamada/chapter02/01.py
+++ kyamada/chapter02/01.py
@@ -2,13 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 fs = 44100
-t = np.arange(fs/2)/fs
-x = np.sin(2*np.pi*t)
+t = np.arange(fs / 2) / fs
+x = np.sin(2 * np.pi * t)
 
-S = np.sum(x)/fs
+S = np.sum(x) / fs
 print(S)
 
-plt.plot(t,x)
+plt.plot(t, x)
 plt.xlabel("Time (s)")
 plt.ylabel("Amplitude")
 plt.show()
