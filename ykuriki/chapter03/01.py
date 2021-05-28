40fb3a732fee8fb4df91023b600621b179609eab
Yui Kuriki <73982429+sternbergia@users.noreply.github.com>
2021-05-29 01:11:47 +0900

    Update 01.py

--- ykuriki/chapter03/01.py
+++ ykuriki/chapter03/01.py
@@ -14,13 +14,3 @@ b = 2 / fs * sum(x * np.sin(2 * np.pi * t))
 
 print(f"amplitude: {np.sqrt(a**2+b**2):.10f}")
 print(f"phase    : {np.arctan2(b,a):.10f}")
-
-t = np.arange(0,fs)/fs
-x = r*np.cos(2*np.pi*t-theta)
-
-a = 2/fs*sum(x*np.cos(2*np.pi*t))
-b = 2/fs*sum(x*np.sin(2*np.pi*t))
-
-print(f'amplitude: {np.sqrt(a**2+b**2):.10f}')
-print(f'phase    : {np.arctan2(b,a):.10f}')
-
