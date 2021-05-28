1f664bae4a9c037e0c89e8e1b2a4d7d746070a38
Yui Kuriki <73982429+sternbergia@users.noreply.github.com>
2021-05-29 01:19:01 +0900

    Update 07.py

--- ykuriki/chapter03/07.py
+++ ykuriki/chapter03/07.py
@@ -1,17 +1,12 @@
 import numpy as np
 
 
-<<<<<<< HEAD
 # パラメータ
-=======
-##パラメータ
->>>>>>> ade158846085480320c13d37c2b26953dd679278
 N = 8
 
 x = np.random.randn(N)
 
 c = np.zeros(N, dtype=complex)
-<<<<<<< HEAD
 for i in range(0, N):
     c[i] = sum(x * np.exp(-1j * 2 * np.pi * i * np.arange(0, N) / N))
 
@@ -20,13 +15,3 @@ X = np.fft.fft(x)
 # print(f'c = {c}')
 # print(f'X = {X}')
 print(np.isclose(c, X))
-=======
-for i in range(0,N):
-    c[i] = sum(x*np.exp(-1j*2*np.pi*i*np.arange(0,N)/N))
-
-X = np.fft.fft(x)
-
-#print(f'c = {c}')
-#print(f'X = {X}')
-print(np.isclose(c,X))
->>>>>>> ade158846085480320c13d37c2b26953dd679278
