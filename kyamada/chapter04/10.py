3a73922e44a9b5d8323792a477c5554747266075
Kenta Yamada <51431248+kurikinton105@users.noreply.github.com>
2021-06-08 11:54:39 +0900

    4章修正

--- kyamada/chapter04/10.py
+++ kyamada/chapter04/10.py
@@ -9,11 +9,24 @@ def create_sound(fs, f):
 
 def estimate_spectral_centroid(t, x, fft_size):
     X = np.fft.fft(x, fft_size)
-    X = abs(X[0 : int(fft_size / 2)])
+    print(X.shape, int(fft_size / 2))  # (65536,) 32768
+    X = abs(X[0 : int(fft_size / 2 + 1)])
+    print(X.shape)  # 32769
     # print("X", X.shape)
-    w = np.arange(int(fft_size / 2)) * fs / fft_size
+    w = np.arange(int(fft_size / 2 + 1)) * fs / fft_size
     spectral_centroid = np.sum(w * X) / np.sum(X)
     print(f"spectral_centroid [Hz]: {spectral_centroid}")
+
+
+def debug_check_array_size():
+    print("Debug")
+    a = np.arange(100)
+    a1 = a[1:50]
+    print(a1)  # 1から49まで
+    print(a1.shape)  # 49
+    # matlabで a(1:50)を行ったときサイズが50になる
+    # >> a = (1:100)  % 1x100
+    # >> a1 = a(1:50) % 1x50
 
 
 if __name__ == "__main__":
@@ -23,3 +36,6 @@ if __name__ == "__main__":
 
     t, x = create_sound(fs, f)
     estimate_spectral_centroid(t, x, fft_size)
+
+    # デバック
+    debug_check_array_size()
